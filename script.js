dab2dcb1ac78dde1a6fec7b8b9afae7c8871c05b
jamile barbosa <105429764+jamilebarbosa@users.noreply.github.com>
2024-06-06 10:36:39 -0300

    ok

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
